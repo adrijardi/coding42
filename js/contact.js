3df80c0a030efb858fa2fce12eeac6e099937adf
Adrian Lopez <adrian@lgpo.net>
2019-12-30 19:07:41 +0000

    Add proper alerts on contact form

--- js/contact.js
+++ js/contact.js
@@ -4,25 +4,27 @@ function sendToAPI(e) {
   const name = document.getElementById("name-input").value;
   const email = document.getElementById("email-input").value;
   const message = document.getElementById("message-input").value;
+  const errorMessage = document.getElementById("error-msg");
+  errorMessage.innerHTML = '';
 
   const nameRegex = /[A-Za-z]{1}[A-Za-z]/;
   if (!nameRegex.test(name)) {
-    alert ("Name can not less than 2 char");
+    errorMessage.appendChild(document.createTextNode("Name can not less than 2 char"));
     return;
   }
 
   if (email === "") {
-    alert ("Please enter your email address");
+    errorMessage.appendChild(document.createTextNode("Please enter your email address"));
     return;
   }
   const emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
   if (!emailRegex.test(email)) {
-    alert ("Please enter valid email address");
+    errorMessage.appendChild(document.createTextNode("Please enter valid email address"));
     return;
   }
 
   if (message === "") {
-    alert ("Please enter a message");
+    errorMessage.appendChild(document.createTextNode("Please enter a message"));
     return;
   }
 
@@ -37,7 +39,11 @@ function sendToAPI(e) {
     method: 'POST',
     body: JSON.stringify(data),
   }).then(function(data) {
-    console.log(data);
+    if(data.ok) {
+      document.getElementById("contact-form").reset();
+    } else {
+      errorMessage.innerHTML = 'Error sending form';
+    }
   });
 
 }
